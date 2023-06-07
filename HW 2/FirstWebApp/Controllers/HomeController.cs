09541e543cd5d84c901fdfbf22de76498bef8022
Unknown <103126971+HE110W0R19@users.noreply.github.com>
2023-06-07 13:34:09 +0300

    add HW 2
    add HW 2

---
+++ HW 2/FirstWebApp/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using FirstWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FirstWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult GamePage([FromForm(Name = "playerName")] string Name)
+        {
+            var newGameModel = new TicTacToeModel(Name);
+            return View(newGameModel);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ HW 2/FirstWebApp/Models/TicTacToeModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace FirstWebApp.Models
+{
+    public class TicTacToeModel
+    {
+        public string playerName { get; set; }
+        public string[] boardRandom;
+        public TicTacToeModel(string Name)
+        {
+            playerName = Name;
+            Random rnd = new Random();
+            boardRandom = new string[9] { " ", " ", " ", " ", " ", " ", " ", " ", " " };
+            for (int i = 0; i < boardRandom.Length; ++i)
+            {
+                if (rnd.Next(0, 10) % 2 == 0)
+                {
+                    boardRandom[rnd.Next(0, 9)] = "O";
+                }
+                else
+                {
+                    boardRandom[rnd.Next(0, 9)] = "X";
+                }
+            }
+        }
+    }
+}
